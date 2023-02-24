6e18f9b3fb8f971855e2fe01edfb45d3d7a1e01e
achi113s <latourgiorgio@gmail.com>
2023-02-24 12:28:35 -0500

    first commit

---
+++ Development/iAmPoor/iAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iAmPoor
+//
+//  Created by Giorgio Latour on 2/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
