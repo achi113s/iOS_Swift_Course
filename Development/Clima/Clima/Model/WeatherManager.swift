ab56762394047757827bb65803c2ea9cbd9564ef
achi113s <latourgiorgio@gmail.com>
2023-03-02 13:57:58 -0500

    weather app

---
+++ Development/Clima/Clima/Model/WeatherManager.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Giorgio Latour on 3/2/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=79a9b649e4253e7153fd7a0834e0fcf3"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        
+    }
+    
+}
---
+++ Development/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Giorgio Latour on 3/2/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
