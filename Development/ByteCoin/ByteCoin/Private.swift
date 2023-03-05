f44713fa1ea78465413be97a094f162ca5259a0b
achi113s <latourgiorgio@gmail.com>
2023-03-05 13:31:35 -0500

    another

---
+++ Development/ByteCoin/ByteCoin/Private.swift
@@ -0,0 +1,13 @@
+//
+//  Private.swift
+//  ByteCoin
+//
+//  Created by Giorgio Latour on 3/5/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct PrivateAPI {
+    let coinAPIKey: String = ""
+}
