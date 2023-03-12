8875915b814ffed318d046d5c446d076aaa26e31
achi113s <latourgiorgio@gmail.com>
2023-03-11 22:44:31 -0500

    diceeswiftui done

---
+++ Development/DiceeSwiftUI/DiceeSwiftUI/DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by Giorgio Latour on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
